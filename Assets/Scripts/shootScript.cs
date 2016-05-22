31f0d31d98c2754c8ff7659a6aedff92e094bb37
Luis <lmpadillagallardo@gmail.com>
2016-05-22 00:12:51 -0800

    Bug fixing

--- Assets/Scripts/shootScript.cs
+++ Assets/Scripts/shootScript.cs
@@ -5,13 +5,14 @@ using System.Collections;
 public class shootScript : MonoBehaviour {
 
     public Rigidbody2D rb;
-    public CircleCollider2D collision;
+    //public CircleCollider2D collision;
     private Vector2 firstPressPos;
     private Vector2 secondPressPos;
     private Vector2 currentSwipe;
     private bool isMoving;
 
     public GameObject[] trashTypes;
+    public GameObject[] trashBins;
     public Text scoreText;
     private int score;
 
@@ -19,7 +20,8 @@ public class shootScript : MonoBehaviour {
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        collision.enabled = false;
+        //collision.enabled = false;
+        disableBinCollision();
         rb.gravityScale = 0;
         isMoving = false;
         if (scoreText.text == "Score Text")
@@ -34,9 +36,17 @@ public class shootScript : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y >= 4)
+        if (scoreText.text == "Score Text")
+            score = 0;
+        else
         {
-            collision.enabled = true;
+            score = int.Parse(scoreText.text);
+        }
+
+        if (transform.position.y >= 3.6)
+        {
+            //collision.enabled = true;
+            enableBinCollision();
         }
         if (Input.GetMouseButtonDown(0))
         {
@@ -48,7 +58,7 @@ public class shootScript : MonoBehaviour {
             currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
             currentSwipe.Normalize();
             rb.gravityScale = 1;
-            rb.AddForce(currentSwipe * 650);
+            rb.AddForce(currentSwipe * 650*5);
             isMoving = true;
         }
     }
@@ -60,7 +70,8 @@ public class shootScript : MonoBehaviour {
             score++;
             setScoreText();
             isMoving = false;
-            collision.enabled = false;
+            //collision.enabled = false;
+            disableBinCollision();
             rb.gravityScale = 0;
             rb.velocity = Vector3.zero;
             rb.angularVelocity = 0;
@@ -68,13 +79,13 @@ public class shootScript : MonoBehaviour {
             gameObject.transform.rotation = new Quaternion(0, 0, 0,0);
             switchTrashType();
         }
-
-        if (other.gameObject.CompareTag("Boundaries"))
+        else if (other.gameObject.CompareTag("Boundaries") || other.gameObject.tag != gameObject.tag)
         {
             score = 0;
             setScoreText();
             isMoving = false;
-            collision.enabled = false;
+            //collision.enabled = false;
+            disableBinCollision();
             rb.gravityScale = 0;
             rb.velocity = Vector3.zero;
             rb.angularVelocity = 0;
@@ -91,24 +102,39 @@ public class shootScript : MonoBehaviour {
     void switchTrashType()
     {
         int type;
-        type = Random.Range(0,2);
+        type = Random.Range(0,3);
         Debug.Log(type);
         gameObject.SetActive(false);
         trashTypes[type].SetActive(true);
+        //collision.enabled = false;
+        disableBinCollision();
 
     }
 
     void enableBinCollision()
     {
+        foreach (GameObject t in trashBins)
+        {
+            EdgeCollider2D[] eCol = t.GetComponents<EdgeCollider2D>();
+            BoxCollider2D bCol = t.GetComponent<BoxCollider2D>();
+            foreach (EdgeCollider2D e in eCol) e.enabled = true;
+            bCol.enabled = true;
+        }
     }
 
     void disableBinCollision()
     {
-
+        foreach (GameObject t in trashBins)
+        {
+            EdgeCollider2D[] eCol = t.GetComponents<EdgeCollider2D>();
+            BoxCollider2D bCol = t.GetComponent<BoxCollider2D>();
+            foreach (EdgeCollider2D e in eCol) e.enabled = false;
+            bCol.enabled = false;
+        }
     }
 
     void OnBecameInvisible()
     {
-        collision.enabled = true;
+        //collision.enabled = true;
     }
 }
